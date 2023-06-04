9f9a7661512f3f22067a5717052c80135ffea154
Nassim's Software <128189209+JSLegendDev@users.noreply.github.com>
2023-06-04 09:26:59 -0400

    added logic to fetch youtube link from user.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const instancesList = [
+
+]
+
+
 async function loadModel() {
     return await tf.loadLayersModel(
      'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json'
@@ -5,11 +10,41 @@ async function loadModel() {
 }
 
 async function loadModelMetaData() {
-    const metaData = await (await fetch('https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/metadata.json')).json()
-    return {
-      maxLen: metaData['max_len'],
-      indexFrom: metaData['index_from'],
-      wordIndex: metaData['word_index'],
-      vocabularySize: metaData['vocabulary_size']
+  const metaData = await (await fetch('https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/metadata.json')).json()
+  return {
+    maxLen: metaData['max_len'],
+    indexFrom: metaData['index_from'],
+    wordIndex: metaData['word_index'],
+    vocabularySize: metaData['vocabulary_size']
+  }
+}
+
+async function main() {
+  let model = null
+  let modelMetadata = null
+  try {
+    model = await loadModel()
+    modelMetadata = await loadModelMetaData()
+  } catch {
+    // TODO: Display an error message to the user
+  }
+
+  const generateRatingBtn = document.getElementById('generateRatingBtn')
+  generateRatingBtn.addEventListener('click', () => {
+    const youtubeSrc = document.getElementById('youtube-src').value
+
+    let videoId = ''
+
+    if (youtubeSrc.includes('youtube')) {
+      videoId = youtubeSrc.split('?v=')[1]
+    } else if (youtubeSrc.includes('youtu.be')) {
+      videoId = youtubeSrc.split('/')[1]
+    } else {
+      videoId = youtubeSrc
     }
+
+
+
+  })
+
 }
